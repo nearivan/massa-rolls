a5c6510995f0e216b268e663f8890182a36f35a3
nearivan <92527194+nearivan@users.noreply.github.com>
2022-07-16 02:46:33 +0300

    Update rollsrebuy.sh

--- rollsrebuy.sh
+++ rollsrebuy.sh
@@ -5,12 +5,18 @@ while true
 do
         balance=$(cd /root/massa/massa-client/ && /root/massa/massa-client/massa-client --pwd $massa_pass wallet_info | grep "Active rolls" | awk '{ print $3 }' && cd)
         int_balance=${balance%%.*}
+        echo "-----"
+        date
         if [ $int_balance -gt "1" ]; then
-                echo "More than 1"
+                echo "Balance: $int_balance"
         elif [ $int_balance -lt "1" ]; then
-                echo "Less than 1"
+                echo "Balance: $int_balance"
+                echo "Buying 1 roll:"
                 buyrolls=$(cd /root/massa/massa-client/ && /root/massa/massa-client/massa-client --pwd $massa_pass buy_rolls $massa_wallet_address 1 0 && cd)
                 echo $buyrolls
         fi
+        echo "Done"
+        echo "-----"
+        echo "Waiting 1 minute"
         sleep 60
 done
